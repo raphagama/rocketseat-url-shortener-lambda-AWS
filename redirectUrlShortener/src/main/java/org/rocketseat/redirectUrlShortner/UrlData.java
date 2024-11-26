54346555682b1d16de6ea4c4b59b8e9d14878ce8
Raphael Gama <raphaelgama19@gmail.com>
2024-11-26 16:46:03 -0300

    Alalinclusão do @NoArgsConstructor na classe URLData

--- redirectUrlShortener/src/main/java/org/rocketseat/redirectUrlShortner/UrlData.java
+++ redirectUrlShortener/src/main/java/org/rocketseat/redirectUrlShortner/UrlData.java
@@ -2,9 +2,11 @@ package org.rocketseat.redirectUrlShortner;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
 public class UrlData {
